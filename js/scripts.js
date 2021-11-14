82ac52e2ce0b00adc269303d55d1dc07827bfab4
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2021-11-14 17:50:33 +0300

    It will add  parseInt and the validation function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-var itemsNeeded= [century, month, day,]
+var itemsNeeded= [century, year, month, day];
+
+//parseint and validate
+function validate() {
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("monthday").value);
+
+  if (century==="") {
+      alert("Enter the century:");
+      return false;
+  } else if(year===""){
+      alert("Enter the year:");
+      return false;
+  } else if(month===""){
+      alert("Enter the month:");
+  } else if(day===""){
+      alert("Enter the day of the month:")
+  } else if(gender===""){
+      alert("Gender input required:");
+  }
+}
