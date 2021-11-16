0a4d42b7e908e1e19ee341083917cebddd6a90f9
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2021-11-16 14:54:16 +0300

    It will add the generation of the Akan names

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,48 @@
-var itemsNeeded= ['century', 'year', 'month', 'day'];
-
-//parseint and validate
-function validate() {
-    century = parseInt(document.getElementById("century").value);
-    year = parseInt(document.getElementById("year").value);
-    month = parseInt(document.getElementById("month").value);
-    dayOfMonth = parseInt(document.getElementById("monthday").value);
-
-  if (century==="") {
-      alert("Enter the century:");
-      return false;
-  } else if(year===""){
-      alert("Enter the year:");
-      return false;
-  } else if(month===""){
-      alert("Enter the month:");
-  } else if(day===""){
-      alert("Enter the day of the month:")
-  } else if(gender===""){
-      alert("Gender input required:");
-  }
-};
-
 //arrays
 let dayOfTheWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 let maleNames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 let femaleNames=['Akosua', 'Adwoa', 'Abenna', 'Akua', 'Yaa', 'Afua', 'Ama'];
 let gender = ["male", "female"];
 
-//calculations
-function calculateTheDay(){
-    getdayOfTheWeek();
-        dayOfTheWeek= ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + days ) % 7;
-        console.log("Day of the week");
-        return (math.floor(dayOfTheWeek));    
+//parseint
+    year = document.getElementById("year");
+    month = document.getElementById("month");
+    dayOfMonth = document.getElementById("dayOfMonth");
+
+//validate
+var button= document.getElementById("register");
+button.addEventListener("submit", validate);
+function validate(e) {
+    e.preventDefault();
+    if(year.value===""){
+      alert("Enter the year:");
+      return false;
+  } else if(month.value===""){
+      alert("Enter the month:");
+  } else if(dayOfMonth.value===""){
+      alert("Enter the day of the month:");
+  } else if(male.checked!==true || female.checked!== true){
+      alert("Gender input required:");
+  }  else{
+    nameGenerator();
+  }
+};
+
+//namegenerator
+function nameGenerator(){
+    let date= `${month.value}-${dayOfMonth.value}-${year.value}`;
+    const d= new Date(date);
+    var dateOfBirth= d.getDay();
+    if (female.checked== true){
+        alert(`Your Akan name is ${femaleNames[dateOfBirth]}`)
+    } else {
+        alert(`Your Akan name is ${maleNames[dateOfBirth]}`)
+    }
 }
 
-if(dayOfTheWeek === 0 || "0" && male){
-    prompt("Your Akan Name is Kwasi");
-} else{
-    prompt("Your Akan Name is Akosua");
-}
+document.getElementById("register").onclick= validate;
 
-if(dayOfTheWeek === 1 || "1" && male){
-    prompt("Your Akan Name is Kwadwo");
-} else{
-    prompt("Your Akan Name is Adwoa");
-}
 
-if(dayOfTheWeek === 2 || "2" && male){
-    prompt("Your Akan Name is Kwabena");
-} else{
-    prompt("Your Akan Name is Abenna");
-}
 
-if(dayOfTheWeek === 3 || "3" && male){
-    prompt("Your Akan Name is Kwaku");
-} else{
-    prompt("Your Akan Name is Akua");
-}
 
-if(dayOfTheWeek === 4 || "4" && male){
-    prompt("Your Akan Name is Yaw");
-} else{
-    prompt("Your Akan Name is Yaa");
-}
 
-if(dayOfTheWeek === 5 || "5" && male){
-    prompt("Your Akan Name is Kofi");
-} else{
-    prompt("Your Akan Name is Afua");
-}
-
-if(dayOfTheWeek === 6 || "6" && male){
-    prompt("Your Akan Name is Kwame");
-} else{
-    prompt("Your Akan Name is Ama");
-}
